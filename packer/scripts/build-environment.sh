9ee31d37f0c482d03cb9aa0dce9e45673264ed14
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-09 20:15:44 -0400

    Added build script to install and configure monitor environment

---
+++ packer/scripts/build-environment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Pull and build NetCap container
+git clone https://github.com/dreadl0ck/netcap.git
+
+pushd netcap
+git checkout v0.5
+docker build . --tag netcap
+popd
+rm -rf netcap
+
+# Pull and build Cisco Mercury container
+git clone https://github.com/cisco/mercury.git
+
+pushd mercury
+docker build . --tag mercury
+popd
+rm -rf mercury
+
+# Pull sFlow containers
+docker pull sflow/prometheus
+docker pull sflow/host-sflow
+docker pull sflow/sflowtool
+
+# download secure-threat-monitor git repository to build containers
+git clone https://github.com/1computerguy/secure-threat-monitor.git
+
+mv secure-threat-monitor/docker/docker-compose.yml .
