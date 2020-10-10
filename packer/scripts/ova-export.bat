f53abd963b17590ec63ce25a5a0841f54fb8f3ab
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-09 20:13:15 -0400

    Use ovftool to export VMware Workstation VM to ova

---
+++ packer/scripts/ova-export.bat
@@ -0,0 +1,3 @@
+echo off
+
+"C:\Program Files (x86)\VMware\VMware Workstation\OVFTool\ovftool.exe" "C:\Users\bscarbrough\Documents\GitHub\secure-threat-monitor\output-vmware-desktop\Secure-Traffic-Monitor.vmx" "C:\Users\bscarbrough\Documents\GitHub\secure-threat-monitor\sec-traf-mon.ova"
