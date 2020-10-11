ad57584ce4940dd452a509e19d78cf734e33d26d
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-10 21:29:44 -0400

    Removed individual docker commands in favor of docker-compose

--- packer/scripts/build-environment.sh
+++ packer/scripts/build-environment.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
 
-# Pull and build NetCap container
-git clone https://github.com/dreadl0ck/netcap.git
-
-pushd netcap
-git checkout v0.5
-docker build . --tag netcap
-popd
-rm -rf netcap
-
-# Pull and build Cisco Mercury container
-git clone https://github.com/cisco/mercury.git
-
-pushd mercury
-docker build . --tag mercury
-popd
-rm -rf mercury
-
-# Pull sFlow containers
-docker pull sflow/prometheus
-docker pull sflow/host-sflow
-docker pull sflow/sflowtool
-
-# Pull Prometheus time-based DB container
-docker pull prom/prometheus
-
-# Pull Grafana container
-docker pull grafana/grafana
+# download and install nDPI
+git clone https://github.com/ntop/nDPI.git
 
 # download secure-threat-monitor git repository to build containers
 git clone https://github.com/1computerguy/secure-threat-monitor.git
 
-mv secure-threat-monitor/docker/docker-compose.yml .
+# Pull git submodules
+pushd secure-threat-monitor
+git submodule update --init
+
