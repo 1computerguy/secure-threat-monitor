d3925a9be4b122da7cb38af799d97b65e578c8d2
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-10 21:28:31 -0400

    Updated to include nDPI build prerequisites

--- packer/scripts/remove-packages.sh
+++ packer/scripts/remove-packages.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
-tdnf --assumeyes remove openldap cyrus-sasl serf subversion apr utf8proc apr-util subversion-perl python2-libs python2 perl perl-DBI perl-YAML perl-CGI git
+tdnf --assumeyes remove \
+                 openldap \
+                 cyrus-sasl \
+                 serf \
+                 subversion \
+                 apr \
+                 utf8proc \
+                 apr-util \
+                 subversion-perl \
+                 python2-libs \
+                 python2 perl \
+                 perl-DBI \
+                 perl-YAML \
+                 perl-CGI \
+                 git \
+                 autogen \
+                 automake \
+                 autoconf \
+                 libtool \
+                 gcc
 
 tdnf clean all
