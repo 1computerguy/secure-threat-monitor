4f6945b1c6b58c0bffb1d58ac652618419166947
1computerguy <31444089+1computerguy@users.noreply.github.com>
2020-10-17 18:09:51 -0400

    Updated build-environment for svn pull of requirements in lieu of submodule

--- packer/scripts/build-environment.sh
+++ packer/scripts/build-environment.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-echo "> Pull secure-threat-monitor git repo and submodules..."
-# download secure-threat-monitor git repository to build containers
-git clone https://github.com/1computerguy/secure-threat-monitor.git
-pushd secure-threat-monitor
-git submodule update --init --recursive
-popd
-
-echo "> Move secure-threat-monitor to monadmin home"
-mv secure-threat-monitor /home/monadmin/
 pushd /home/monadmin
-chown -R monadmin:monadmin secure-threat-monitor
+
+echo "> Download required docker and resource directories..."
+# download secure-threat-monitor git repository to build containers
+svn export https://github.com/cisco/mercury/trunk/resources
+svn export https://github.com/1computerguy/secure-threat-monitor/trunk/docker
+
+echo "> Set permissions for monadmin resources..."
+chown -R monadmin:monadmin {docker,resources}
 
 echo "> Move docker-compose to monadmin home directory..."
-mv secure-threat-monitor/docker/docker-compose.yml /home/monadmin/docker-compose.yml
-
-mkdir -p /var/log/checkip
+mv /home/monadmin/docker/docker-compose.yml /home/monadmin/docker-compose.yml
+mkdir -p /var/log/{checkip,pmercury}
 
 echo "> Building containers with docker-compose..."
-docker-compose build -d
-popd
+docker-compose build --force-rm
+docker-compose pull
